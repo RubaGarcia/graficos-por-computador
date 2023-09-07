e7b0fa6640a101a9e589d9751a8ec5d74e2ef208
Ruben <rgm177@alumnos.unican.es>
2023-09-07 14:24:49 +0200

    primera iteracion

---
+++ practica1.py
@@ -0,0 +1,59 @@
+import tkinter as tk
+
+# Función para cambiar el tamaño de los puntos en el Canvas
+def cambiar_tamanio_punto():
+    nuevo_tamanio = float(entry_tamanio.get())
+    canvas.itemconfig(lapiz, width=nuevo_tamanio)
+
+# Función para cambiar el color de los puntos en el Canvas
+def cambiar_color_punto():
+    nuevo_color = color_var.get()
+    canvas.itemconfig(lapiz, fill=nuevo_color)
+
+# Función para dibujar en el Canvas cuando se hace clic en él
+def dibujar(event):
+    x, y = event.x, event.y
+    tamanio = float(canvas.itemcget(lapiz, "width"))
+    color = canvas.itemcget(lapiz, "fill")
+    canvas.create_oval(x - tamanio, y - tamanio, x + tamanio, y + tamanio, fill=color, outline=color)
+
+# Crear la ventana principal
+root = tk.Tk()
+root.title("Aplicación de Dibujo")
+
+# Crear el Canvas para dibujar
+canvas = tk.Canvas(root, width=400, height=400, bg="white")
+canvas.pack(side=tk.LEFT, padx=10, pady=10)
+
+# Crear el lápiz (inicialmente negro)
+lapiz = canvas.create_oval(0, 0, 5, 5, fill="black")
+
+# Configurar eventos de dibujo en el Canvas
+canvas.bind("<Button-1>", dibujar)
+
+# Crear un marco para los botones
+frame = tk.Frame(root)
+frame.pack(side=tk.RIGHT, padx=10, pady=10)
+
+# Crear una entrada para cambiar el tamaño del punto
+label_tamanio = tk.Label(frame, text="Tamaño del punto:")
+label_tamanio.pack()
+entry_tamanio = tk.Entry(frame)
+entry_tamanio.pack()
+btn_cambiar_tamanio = tk.Button(frame, text="Cambiar Tamaño", command=cambiar_tamanio_punto)
+btn_cambiar_tamanio.pack()
+
+# Crear una lista de opciones de colores
+colores = ["black", "red", "green", "blue", "orange", "purple", "pink"]
+color_var = tk.StringVar(value="black")
+
+# Crear una etiqueta y un menú desplegable para cambiar el color del punto
+label_color = tk.Label(frame, text="Color del punto:")
+label_color.pack()
+menu_color = tk.OptionMenu(frame, color_var, *colores)
+menu_color.pack()
+btn_cambiar_color = tk.Button(frame, text="Cambiar Color", command=cambiar_color_punto)
+btn_cambiar_color.pack()
+
+# Ejecutar la aplicación
+root.mainloop()
